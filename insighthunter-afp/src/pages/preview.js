4c4db39b87d24ca6ff68dd1709e32e83814fdd61
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-14 00:32:40 -0500

    Update and rename src/pages/preview.js to insighthunter-afp/src/pages/preview.js

--- insighthunter-afp/src/pages/preview.js
+++ insighthunter-afp/src/pages/preview.js
@@ -3,7 +3,7 @@ import * as React from "react"
 export default function PreviewPage() {
   return (
     <main style={{ padding: "2rem", color: "#00ffff", background: "#0b0f1a" }}>
-      <h1>Simulation Preview</h1>
+      <h1>Simulation Preview</h1>sr
       <p>Preview seeded simulations will be displayed here.</p>
     </main>
   )
