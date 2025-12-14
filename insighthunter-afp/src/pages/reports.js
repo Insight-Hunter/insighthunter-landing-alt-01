965dcbdd1acaf299899e1e4d112dedf13f760c3a
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-14 00:58:19 -0500

    Update reports.js

--- insighthunter-afp/src/pages/reports.js
+++ insighthunter-afp/src/pages/reports.js
@@ -2,6 +2,7 @@ import * as React from "react"
 
 export default function ReportsPage() {
   return (
+    <Layout>
     <main style={{ padding: "2rem", color: "#00ffff", background: "#0b0f1a" }}>
       <h1>Reports</h1>
       <p>List of generated reports will appear here.</p>
@@ -9,5 +10,6 @@ export default function ReportsPage() {
         <li><a href="/reports/dec-12.pdf">Download Dec 12 Report</a></li>
       </ul>
     </main>
+  </Layout>
   )
 }
