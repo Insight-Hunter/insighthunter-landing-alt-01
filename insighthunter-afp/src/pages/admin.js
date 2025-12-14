460c1b846b286c0a5557967d48bc288749b83be4
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-14 00:49:43 -0500

    Update admin.js

--- insighthunter-afp/src/pages/admin.js
+++ insighthunter-afp/src/pages/admin.js
@@ -1,10 +1,13 @@
 import * as React from "react"
+import Layout from "../components/Layout"
 
 export default function AdminPage() {
   return (
     <main style={{ padding: "2rem", color: "#00ffff", background: "#0b0f1a" }}>
+     <Layout>
       <h1>Admin</h1>
       <p>Administrative controls and audit logs.</p>
+     </Layout>
     </main>
   )
 }
