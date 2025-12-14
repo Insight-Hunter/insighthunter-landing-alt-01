ce88cf03999b11a3752f77e691478f41a49ae409
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-14 01:03:19 -0500

    Update wizard.js

--- insighthunter-afp/src/pages/wizard.js
+++ insighthunter-afp/src/pages/wizard.js
@@ -1,10 +1,13 @@
 import * as React from "react"
+import Layout from "../compoments/Layout"
 
 export default function WizardPage() {
   return (
+    <Layout>
     <main style={{ padding: "2rem", color: "#00ffff", background: "#0b0f1a" }}>
       <h1>Wizard</h1>
       <p>Interactive onboarding wizard goes here.</p>
     </main>
+  </Layout>
   )
 }
