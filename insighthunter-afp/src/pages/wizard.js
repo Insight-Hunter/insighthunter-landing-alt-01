5baa8cb3d8aa9adaae657940dc84f29104e76d5c
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-14 00:26:00 -0500

    Create wizard.js

---
+++ insighthunter-afp/src/pages/wizard.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+export default function WizardPage() {
+  return (
+    <main style={{ padding: "2rem", color: "#00ffff", background: "#0b0f1a" }}>
+      <h1>Wizard</h1>
+      <p>Interactive onboarding wizard goes here.</p>
+    </main>
+  )
+}
