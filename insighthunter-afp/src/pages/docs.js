d989a131f91a4472836f888ff7919328532fd4a2
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-14 00:33:50 -0500

    Create docs.js

---
+++ insighthunter-afp/src/pages/docs.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+export default function DocsPage() {
+  return (
+    <main style={{ padding: "2rem", color: "#00ffff", background: "#0b0f1a" }}>
+      <h1>Docs</h1>
+      <p>Documentation hub for InsightHunter.</p>
+    </main>
+  )
+}
