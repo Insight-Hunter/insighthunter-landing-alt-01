24de4449c6f47faccab3cf5c1a5909da4ffc0eb9
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-14 00:35:57 -0500

    Create support.js

---
+++ insighthunter-afp/src/pages/support.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+export default function SupportPage() {
+  return (
+    <main style={{ padding: "2rem", color: "#00ffff", background: "#0b0f1a" }}>
+      <h1>Support</h1>
+      <p>Contact and troubleshooting resources.</p>
+    </main>
+  )
+}
